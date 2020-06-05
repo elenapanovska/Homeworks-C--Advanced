c4dd36c912f71e4b37c65a77ddebe3a2ab9c286e
Elena <panovska.elena.ep@gmail.com>
2020-06-05 17:11:00 +0200

    class 01 & 02 exercises

---
+++ Class02.QuizApp/Class02.QuizApp.Business/Services/ValidationService.cs
@@ -0,0 +1,56 @@
+﻿using Class02.QuizApp.Data.Data;
+using Class02.QuizApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class02.QuizApp.Business.Services
+{
+    public class ValidationService 
+    {
+
+        public bool DoesUserNameExist(List<User> users, string userName)
+        {
+            foreach (var userN in users)
+            {
+                if (userN.UserName == userName)
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+
+        public bool UsernamePasswordMatch(User user, string username, string password)
+        {
+            if(user.UserName == username && user.Password == password)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool ThreeAttemptsPass(User user, string password)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                if(user.Password != password)
+                {
+                    Console.WriteLine($"Wrong password you have 3 attepts otherwise the app wil close");
+                    Console.Write("Password: ");
+                    password = Console.ReadLine();
+
+                    if(i == 3 && user.Password != password)
+                    {
+                        return false;
+                    };
+                }
+                
+            }
+            return true;
+            
+                    
+        }
+    }
+}
